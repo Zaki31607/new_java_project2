e481dc24e1a1dbcea554f31daa16439ad15a9c53
Zurab <kakashvili.zurab@mail.ru>
2024-09-24 15:40:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология");
+        System.out.println("2+3");
+        System.out.println(2 + 3);
+
+        int speed = 17;
+        System.out.println(speed);
+    }
+}
